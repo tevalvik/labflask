a33a9b8b041a65e7e03d07f64c845b0e582897a8
Trond Eivind Valvik <sneglen1+github@gmail.com>
2019-02-12 17:36:47 +0100

    fixed db for login.

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -44,7 +44,7 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        db = db.get_db()
+        db = get_db()
         error = None
         user = db.execute('SELECT * FROM user WHERE username = ?',
                           (username,)).fetchone()
